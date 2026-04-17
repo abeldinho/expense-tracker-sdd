942b770d50dc5a9b66a2fd3378f42e4b9602e488
Abel Aracena <aracena.abel@gmail.com>
2026-04-17 19:06:01 -0400

    chore: se agrega la forma para crear un super user en render

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD python manage.py migrate && python manage.py runserver 0.0.0.0:$PORT
+CMD python manage.py migrate && python manage.py createadmin && python manage.py runserver 0.0.0.0:$PORT
---
+++ expenses/management/commands/createadmin.py
@@ -0,0 +1,25 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not password:
+            self.stdout.write(self.style.WARNING("Credenciales no definidas"))
+            return
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS("Superusuario creado"))
+        else:
+            self.stdout.write("Superusuario ya existe")
