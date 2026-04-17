d4fb98394f0c9bcb39b795908b8600f1ac1dd4cc
Abel Aracena <aracena.abel@gmail.com>
2026-04-17 18:32:06 -0400

    chore: se cambia el debug en setting para que se pueda ver bien el admin de django

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k14=r*42h*#*6(ote1z#x5d07z%)e8c8ej7^k0fjvm&+qr)+-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
