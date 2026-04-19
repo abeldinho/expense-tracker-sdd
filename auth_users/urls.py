94858f48200143a0d53a9fc88d48e2cde81e3fa1
Abel Aracena <aracena.abel@gmail.com>
2026-04-18 22:26:28 -0400

    Arrgeglo: se soluciona problema con el logout.

--- auth_users/urls.py
+++ auth_users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,6 +6,10 @@ app_name = "users"
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth_users/login.html',next_page='expenses:ingresar_expense'),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='auth_users:login'),name='logout'),
+    path(
+    'logout/',
+    auth_views.LogoutView.as_view(next_page=reverse_lazy('users:login')),
+    name='logout'
+),
     path('signup/', views.signup_view,name='register'),
     ]
